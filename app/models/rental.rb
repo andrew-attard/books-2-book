36edb9e26455caf9043b9351f9f7a459fbe4f9e0
vincent de leonardis <vincent.deleo@gmail.com>
2024-05-14 16:58:28 +0900

    rentals basic page done

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -2,6 +2,7 @@ class Rental < ApplicationRecord
   belongs_to :ownership
   # belongs_to :books
   belongs_to :user
+  has_one :book, through: :ownership
 
   validates :status, presence: true
   validates :start_date, presence: true
