d6f86c1e995c247354d976a7ad30b08cc1f90b93
Moritz Lange <moritz.langebuy@web.de>
2024-05-15 14:50:53 +0900

    new home design

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @books = Book.all.select { |book| book.title.split.size <= 3 }
   end
 end
