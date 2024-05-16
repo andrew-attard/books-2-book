d12bb31594581564ec898435e800c775c8d7599c
Yoana Chang <yewonyoana@gmail.com>
2024-05-16 16:42:11 +0900

    Added 'end' in the routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-  
+
   resources :books, only: %i[index show] do
     resources :ownerships, only: %i[new create]
   end
@@ -40,4 +40,5 @@ Rails.application.routes.draw do
 
   # Search route
   get '/search', to: 'search#index', as: 'search'
+end
 end
