2eb1617c8ab53c7197d48a34f8e13711ca15fd6e
vincent de leonardis <vincent.deleo@gmail.com>
2024-05-17 15:40:12 +0900

    reverte change

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,12 +1,11 @@
 class Rental < ApplicationRecord
   belongs_to :ownership
+  # belongs_to :books
   belongs_to :user
   has_one :book, through: :ownership
 
   validates :status, presence: true
-  enum status: { pending: 'pending', accepted: 'accepted', rejected: 'rejected' }, _default: :pending
+  enum status: { pending: 'pending', accepted: 'accepted', rejected: 'rejected' }, _default: :pending # to be used later in the owner journey
   validates :start_date, presence: true
   validates :end_date, presence: true
-  validate :start_date_before_end_date
-
 end
