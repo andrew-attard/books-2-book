e76bf07bd8001ae520c328bc6239d8c240b6ade1
vincent de leonardis <vincent.deleo@gmail.com>
2024-05-17 15:14:39 +0900

    confirm rent safety

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,11 +1,12 @@
 class Rental < ApplicationRecord
   belongs_to :ownership
-  # belongs_to :books
   belongs_to :user
   has_one :book, through: :ownership
 
   validates :status, presence: true
-  enum status: { pending: 'pending', accepted: 'accepted', rejected: 'rejected' }, _default: :pending # to be used later in the owner journey
+  enum status: { pending: 'pending', accepted: 'accepted', rejected: 'rejected' }, _default: :pending
   validates :start_date, presence: true
   validates :end_date, presence: true
+  validate :start_date_before_end_date
+
 end
